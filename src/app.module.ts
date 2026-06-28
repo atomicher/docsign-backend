db24605ef4e39a6897df9cc1a5b58200d508f1f4
Анна Чернявська <146863432+atomicher@users.noreply.github.com>
2026-06-28 23:23:15 +0300

    Refactor database connection and SSL settings
    Refactor database connection configuration and update SSL settings.

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,24 +13,27 @@ import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
     TypeOrmModule.forRootAsync({
-  useFactory: () => ({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432'),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-    timezone: '+03:00',
-  }),
-}),
+      useFactory: () => ({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
       serveRoot: '/files',
     }),
+
     AuthModule,
     UsersModule,
     DocumentsModule,
